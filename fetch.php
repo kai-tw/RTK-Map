b6befc9220637ef2ba03e258367f7a08ffde492f
chengkai505 <wuvincentck505@gmail.com>
2022-05-06 23:35:45 +0800

    Deployed on GitHub page

---
+++ fetch.php
@@ -0,0 +1,53 @@
+<?php
+/* 
+	ini_set: prevent float precision bug.
+	more info: https://stackoverflow.com/questions/43865885/how-to-json-encode-float-values-in-php-7-1-1
+*/
+ini_set('serialize_precision', -1);
+
+define('RESOURCE_URL', 'https://data.nhi.gov.tw/resource/Nhi_Fst/Fstdata.csv');
+
+header('Content-Type: application/json; charset=utf-8');
+
+$data = ['type'=>'FeatureCollection', 'features'=>[]];
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, RESOURCE_URL);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+
+$mem = fopen('php://memory', 'r+');
+fwrite($mem, $result);
+rewind($mem);
+fgets($mem); /* skip first row */
+while (($row = fgets($mem)) !== false) {
+	$row = explode(',', $row);
+	$from = ['０', '１', '２', '３', '４', '５', '６', '７', '８', '９', '－'];
+	$to = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'];
+	$row[2] = str_replace($from, $to, $row[2]);
+	$feature = [
+					'type'=>'Feature',
+					'properties'=>[
+						'id'		=>	$row[0],
+						'name'		=>	$row[1],
+						'address'	=>	$row[2],
+						'phone'		=>	$row[5],
+						'brand'		=>	$row[6],
+						'stock'		=>	(int)$row[7],
+						'updateTime'=>	$row[8],
+						'note'		=>	$row[9]
+					],
+					'geometry'=>[
+						'type'			=>	'Point',
+						'coordinates'	=>	[
+							floatval($row[3]),
+							floatval($row[4])
+						]
+					]
+				];
+	$data['features'][] = $feature;
+}
+fclose($mem);
+echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
---
+++ main.js
@@ -0,0 +1,158 @@
+"use strict";
+(function(){
+	/* Constant */
+	const DEFAULT_WEIGHT = 3;
+	const DEFAULT_RADIUS = 15;
+	const FOCUS_WEIGHT = 5;
+	const FOCUS_RADIUS = 20;
+	const FOCUS_COLOR = "#351932";
+	
+	/* State var define */
+	let prevClickedMarker = null;
+	
+	/* Load leaflet script from CDN */
+	let script = document.createElement("script");
+	script.src = "//unpkg.com/leaflet@1.8.0/dist/leaflet.js";
+	script.defer = true;
+	script.addEventListener("load", function() {
+		/* Start to draw the map */
+		let map = L.map("app", {
+			renderer: L.canvas(),
+			preferCanvas: true,
+			zoomControl: false,
+			center: [23.973875, 120.982024],
+			zoom: 7,
+			minZoom: 6,
+			maxZoom: 18,
+			maxBounds: L.latLngBounds(L.latLng(16, 111), L.latLng(30, 131))
+		});
+		L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png").addTo(map);
+		
+		/* Load geo JSON */
+		let xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", function() {
+			/* Load successfully */
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				let dataLayer = L.geoJSON(JSON.parse(xhr.responseText), {
+					pointToLayer: function(feature, latLng) {
+						/* Define the style of markers */
+						let markerOption = {
+							alt: feature.properties.name,
+							radius: DEFAULT_RADIUS,
+							color: markerColorDecision(feature),
+							fillColor: markerColorDecision(feature),
+							weight: DEFAULT_WEIGHT,
+							opacity: 1,
+							fillOpacity: 0.75
+						};
+						let marker = L.circleMarker(latLng, markerOption);	/* Create circle marker */
+						marker.on("click", function(event) {				/* Marker click event binding */
+							informationUpdate(event.target.feature.properties);
+							if (!info.classList.contains("show")) {
+								info.classList.add("show");
+							}
+							info.dataset.pharmacyId = event.target.feature.properties.id;
+							if (prevClickedMarker) {
+								prevClickedMarker.setStyle({color: markerColorDecision(prevClickedMarker.feature), radius: DEFAULT_RADIUS, weight: DEFAULT_WEIGHT});
+							}
+							event.target.setStyle({color: FOCUS_COLOR, radius: FOCUS_RADIUS, weight: FOCUS_WEIGHT});
+							prevClickedMarker = event.target;
+						});
+						return marker;
+					}
+				}).addTo(map);
+				
+				/* Periodic update */
+				window.setInterval(function() {
+					let client = new XMLHttpRequest();
+					client.addEventListener("load", function() {
+						if (client.readyState == 4 && client.status == 200) {
+							let data = JSON.parse(client.responseText);
+							let info = document.getElementById("info");
+							let tags = ["address", "phone", "brand", "stock", "updateTime", "note"];
+							/* Information panel update */
+							if (info.dataset.pharmacyId) {
+								data.features.forEach(function(feature) {
+									let properties = feature.properties;
+									if (info.dataset.pharmacyId === properties.id) {
+										informationUpdate(properties);
+										return;
+									}
+								});
+							}
+							/* Markers update */
+							map.eachLayer(function(layer) {
+								if (layer.feature) {
+									let markerProperties = layer.feature.properties;
+									let updated = false;
+									data.features.forEach(function(feature) {
+										let properties = feature.properties;
+										if (markerProperties.id === properties.id) {
+											for (let i = 0; i < tags.length; i++) {
+												markerProperties[tags[i]] = properties[tags[i]];
+											}
+											updated = true;
+											return;
+										}
+									});
+									if (!updated) {
+										layer.remove();	// remove the pharmacy which was out of stock
+									}
+								}
+							});
+						}
+					});
+					client.open("get", "//kai-tw.herokuapp.com/RTK-Map/fetch.php");
+					client.send();
+				}, 30 * 1000);
+			}
+		});
+		xhr.open("get", "//kai-tw.herokuapp.com/RTK-Map/fetch.php");
+		xhr.send();
+		
+		/* Buttons event listener binding */
+		document.getElementById("infoPanelClose").addEventListener("click", function() {
+			let info = document.getElementById("info");
+			if (info.classList.contains("show")) {
+				info.classList.remove("show");
+			}
+			if (prevClickedMarker) {
+				prevClickedMarker.setStyle({color: markerColorDecision(prevClickedMarker.feature), radius: DEFAULT_RADIUS, weight: DEFAULT_WEIGHT});
+				prevClickedMarker = null;
+			}
+		});
+		document.getElementById("mapZoomIn").addEventListener("click", function() {
+			map.zoomIn();
+		});
+		document.getElementById("mapZoomOut").addEventListener("click", function() {
+			map.zoomOut();
+		});
+		document.getElementById("panelToggle").addEventListener("click", function() {
+			document.getElementById("panel").classList.toggle("hidden");
+			this.classList.toggle("active");
+		});
+	});
+	document.body.appendChild(script);
+}());
+
+function informationUpdate(properties) {
+	let info = document.getElementById("info");
+	document.getElementById("pharmacyName").innerText = properties.name;
+	let rows = info.getElementsByClassName("row");
+	let tags = ["address", "phone", "brand", "stock", "updateTime", "note"];
+	for (let i = 0; i < rows.length; i++) {
+		rows[i].getElementsByClassName("content")[0].innerText = properties[tags[i]];
+	}
+}
+function markerColorDecision(feature) {
+	const DAILY_MAX_STOCK = 78;
+	if (feature.properties.stock >= 0.5 * DAILY_MAX_STOCK) {
+		return "#107879";
+	}
+	else if (feature.stock >= 0.2 * DAILY_MAX_STOCK) {
+		return "#e67f22";
+	}
+	else {
+		return "#e91e3a";
+	}
+}
